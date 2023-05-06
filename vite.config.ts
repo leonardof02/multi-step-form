62a1001307452c1f0e6357c26726d4f1866ad81e
leonardof02 <leonardoarencibia2002@gmail.com>
2023-05-05 23:31:23 -0400

    vite.config updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/frontendmentor-solutions/multi-step-form",
+    base: "/multi-step-form/",
 })
